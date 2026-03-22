bc9912c37cc2e62e7b68c6d56ab7ff97e941eebf
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-22 11:51:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Triper/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
