394ca6b343bd5299d84a02cebd0bbfc5fd1b929f
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-22 14:43:38 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   base: "/Triper/",
   plugins: [react()],
   publicDir: "public",
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "TriperStripe",
+      fileName: () => "triper.js",
+      formats: ["es", "umd"]
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react-router-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react-router-dom": "ReactRouterDOM"
+        }
+      }
+    }
+  }
 })
