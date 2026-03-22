389333855f3227d630bc8963d85d642806f444f1
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-22 15:41:00 +0000

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function NotFound {
+export default function NotFound() {
   console.error("404 - Not Found");
   
   return (
